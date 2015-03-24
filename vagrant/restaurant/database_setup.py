0801a61b4c7a8802f955e5520f99dbec6aee67d9
hotpepper <heyseth@hotmail.com>
2015-03-24 18:50:23 -0200

    adds database setup

---
+++ vagrant/restaurant/database_setup.py
@@ -0,0 +1,20 @@
+import sys
+from sqlalchemy import Column, ForeignKey, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+class Restaurant(Base):
+	__tablename__ = 'restaurant'
+
+class MenuItem(Base):
+	__tablename__ = 'menu_item'
+		
+
+
+
+######end of file ######
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.create_all(engine)
