17ca919d4bc8bcb217e5fb58010b4904b448e985
hotpepper <heyseth@hotmail.com>
2015-04-04 14:31:25 -0400

    adds REST api for json of menus and menu items

--- vagrant/restaurant/project.py
+++ vagrant/restaurant/project.py
@@ -1,6 +1,6 @@
 
 #http://localhost:5000/restaurants/2/
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
 app = Flask(__name__)
 
 from sqlalchemy import create_engine
@@ -13,6 +13,18 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
+#making api edpoint (get)
+@app.route('/restaurants/<int:restaurant_id>/menu/JSON')
+def restaurantMenuJSON(restaurant_id):
+    restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
+    items = session.query(MenuItem).filter_by(restaurant_id = restaurant_id).all()
+    return jsonify(MenuItems=[i.serialize for i in items])
+
+@app.route('/restaurants/<int:restaurant_id>/menu/<int:menu_id>/JSON')
+def restaurantMenuItemJSON(restaurant_id, menu_id):
+    restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
+    item = session.query(MenuItem).filter_by(id = menu_id).one() 
+    return jsonify(MenuItem=[item.serialize])
 
 @app.route('/')
 @app.route('/restaurants/<int:restaurant_id>/')
