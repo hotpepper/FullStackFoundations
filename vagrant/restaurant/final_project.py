a57ea766f2c8c8ae8a55b51c640b9f609d7296bf
hotpepper <heyseth@hotmail.com>
2015-04-11 15:22:56 -0400

    adds db connection to new restaurant

--- vagrant/restaurant/final_project.py
+++ vagrant/restaurant/final_project.py
@@ -1,6 +1,14 @@
 #http://localhost:5000/restaurants/
 from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
 app = Flask(__name__)
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Restaurant, MenuItem
+
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
 
 #Fake Restaurants
 restaurant = {'name': 'The CRUDdy Crab', 'id': '1'}
@@ -15,13 +23,30 @@ item =  {'name':'Cheese Pizza','description':'made with fresh cheese','price':'$
 
 @app.route('/')
 @app.route('/restaurants/')
-def restaurantMenu():
-    return render_template('restaurants.html', restaurants=restaurants)
+def showRestaurants():
+	restaurants = session.query(Restaurant).all()
+	return render_template('restaurants.html', restaurants=restaurants)
 
-
-@app.route('/restaurants/new')
+@app.route('/restaurants/new', methods=['GET','POST'])
 def newRestaurant():
-    return render_template('new_restaurant.html')
+	if request.method == 'POST':
+		#making new restaurant
+		addNewRestaurant = Restaurant(name = request.form['name'])
+		session.add(addNewRestaurant)
+		session.commit()
+		flash("new restaurant created!")
+		return redirect(url_for('showRestaurants'))
+	else: 
+		#landing on page
+		return render_template('new_restaurant.html')
+
+
+        
+
+
+
+
+
 
 @app.route('/restaurants/<int:restaurant_id>/edit')
 def editRestaurant(restaurant_id):
@@ -49,5 +74,6 @@ def deleteMenuItem(restaurant_id, menu_id):
     return render_template('restaurant_menu_delete.html', restaurant_id=restaurant_id, menu_id=menu_id, item=item)
 
 if __name__ == '__main__':
-    app.debug = True
-    app.run(host = '0.0.0.0', port = 5000)
+	app.secret_key = 'super_secrect_key'
+	app.debug = True
+	app.run(host = '0.0.0.0', port = 5000)
