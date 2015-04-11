3b3fb5b0a97eecdddfee019a3955f6821a1f9686
hotpepper <heyseth@hotmail.com>
2015-04-11 14:45:21 -0400

    adds wireframe forms to the templates

--- vagrant/restaurant/final_project.py
+++ vagrant/restaurant/final_project.py
@@ -2,12 +2,21 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
 app = Flask(__name__)
 
+#Fake Restaurants
+restaurant = {'name': 'The CRUDdy Crab', 'id': '1'}
+
+restaurants = [{'name': 'The CRUDdy Crab', 'id': '1'}, {'name':'Blue Burgers', 'id':'2'},{'name':'Taco Hut', 'id':'3'}]
+
+#Fake Menu Items
+items = [ {'name':'Cheese Pizza', 'description':'made with fresh cheese', 'price':'$5.99','course' :'Entree', 'id':'1'}, {'name':'Chocolate Cake','description':'made with Dutch Chocolate', 'price':'$3.99', 'course':'Dessert','id':'2'},{'name':'Caesar Salad', 'description':'with fresh organic vegetables','price':'$5.99', 'course':'Entree','id':'3'},{'name':'Iced Tea', 'description':'with lemon','price':'$.99', 'course':'Beverage','id':'4'},{'name':'Spinach Dip', 'description':'creamy dip with fresh spinach','price':'$1.99', 'course':'Appetizer','id':'5'} ]
+item =  {'name':'Cheese Pizza','description':'made with fresh cheese','price':'$5.99','course' :'Entree'}
+
 
 
 @app.route('/')
 @app.route('/restaurants/')
 def restaurantMenu():
-    return render_template('restaurants.html')
+    return render_template('restaurants.html', restaurants=restaurants)
 
 
 @app.route('/restaurants/new')
@@ -16,16 +25,16 @@ def newRestaurant():
 
 @app.route('/restaurants/<int:restaurant_id>/edit')
 def editRestaurant(restaurant_id):
-    return render_template('edit_restaurant.html', restaurant_id=restaurant_id)
+    return render_template('edit_restaurant.html', restaurant_id=restaurant_id, restaurant=restaurant)
 
 @app.route('/restaurants/<int:restaurant_id>/delete')
 def deleteRestaurant(restaurant_id):
-    return render_template('delete_restaurant.html', restaurant_id=restaurant_id)
+    return render_template('delete_restaurant.html', restaurant_id=restaurant_id, restaurant=restaurant)
 
 @app.route('/restaurants/<int:restaurant_id>/')
 @app.route('/restaurants/<int:restaurant_id>/menu')
 def showMenu(restaurant_id):
-    return render_template('restaurant_menu.html', restaurant_id=restaurant_id)
+    return render_template('restaurant_menu.html', restaurant_id=restaurant_id, items = items)
 
 @app.route('/restaurants/<int:restaurant_id>/menu/new')
 def newMenuItem(restaurant_id):
@@ -33,11 +42,11 @@ def newMenuItem(restaurant_id):
 
 @app.route('/restaurants/<int:restaurant_id>/menu/<int:menu_id>/edit')
 def editMenuItem(restaurant_id, menu_id):
-    return render_template('restaurant_menu_edit.html', restaurant_id=restaurant_id, menu_id=menu_id)
+    return render_template('restaurant_menu_edit.html', restaurant_id=restaurant_id, menu_id=menu_id, item=item)
 
 @app.route('/restaurants/<int:restaurant_id>/menu/<int:menu_id>/delete')
 def deleteMenuItem(restaurant_id, menu_id):
-    return render_template('restaurant_menu_delete.html', restaurant_id=restaurant_id, menu_id=menu_id)
+    return render_template('restaurant_menu_delete.html', restaurant_id=restaurant_id, menu_id=menu_id, item=item)
 
 if __name__ == '__main__':
     app.debug = True
